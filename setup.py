822317a35146d01d2369babb77b9f15599956577
Mahdi Zarrintareh <mza2rintareh@gmail.com>
2021-06-16 16:14:29 +0430

    Change name of lib

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with io.open("src/bournix/__init__.py", "rt", encoding="utf8") as f:
     version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
 setup(
-    name="bournix",
+    name="cutter-plier",
     version=version,
     description="Bournix team utils for django",
     long_description=readme,
